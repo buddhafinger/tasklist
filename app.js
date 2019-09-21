fa6e078f0ab9429577e3828aa0099b7c74ed656a
buddhafinger <garry.stevens99@gmail.com>
2019-09-21 14:07:40 +0100

    added files

---
+++ app.js
@@ -0,0 +1,18 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.selection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+
+
+
+
+//LoadEvent Listerners
+function loadEventListerners();
+
+
+
+
