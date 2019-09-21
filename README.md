f1afed89d4493c0a9712a0b39def0961400798ad
buddhafinger <garry.stevens99@gmail.com>
2019-09-21 16:20:17 +0100

    updated readme and added functionailty to append to list

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-JS Tasklist tutorial
+Tasklist tutorial
+
+App.js
+
+Define component variables with querySelector
+
+Create function to listen for events
+
+Create function for firing event on form submission 'addTask'
+
+Create function to generate prompt for empty task submission - prevented default behavior so form does not submit.
+
+Loaded Event Listerners function.
--- app.js
+++ app.js
@@ -5,14 +5,49 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
+loadEventListerners();
 
+//LoadEvent Listerners - Listen for submit event and add task.
+function loadEventListerners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
 
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');  
+  }
 
+  // Create li element
+  const li = document.createElement('li');
 
+  // Add class - use collection-item to ensure formating is picked up in materialize.
 
-//LoadEvent Listerners
-function loadEventListerners();
+  li.className = 'collection-item'; 
+  //Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
 
+  //Create new link element.
+  const link = document.createElement('a');
 
+  //Add Class
+  link.className = 'delete-item secondary-content';
 
+  //Add icon html  
+  link.innerHTML = '<i class="fa fa-remove"></i>';
 
+  //Append the link to li
+  li.appendChild(link);
+
+  // Append link to ul
+  taskList.appendChild(li);
+
+  // Clear input 
+  taskInput.value = '';
+
+console.log(li);
+
+//prevent default form submit action.
+//e.preventDefault();
+}
